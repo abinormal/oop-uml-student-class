1802754c854989c714b7a93b25ed16e50ba3e353
Abi Sidford <asidford@gmail.com>
2021-09-16 00:30:00 +0100

    Create Main, create new instance of Student and increase a grade.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Student peter = new Student("Peter", GRADE.B);
+        peter.increaseGrade();
+        peter.increaseGrade();
+        System.out.println(peter.getName()+ " has grade " + peter.getGrade());
+
+        // Can't get secretNickname
+        // String secret = peter.secretNickName;
+    }
+
+}
